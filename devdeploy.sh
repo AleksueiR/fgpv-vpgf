6d4604ec6465e11e67c627d030716a0dd0211e28
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2016-02-04 09:15:16 -0500

    blah

--- devdeploy.sh
+++ devdeploy.sh
@@ -1,12 +1,12 @@
 SSHSRV="travis@fgpv.cloudapp.net"
 DESTDIR="/home/travis/www"
 
-if [ "$TRAVIS_REPO_SLUG" == "fgpv-vpgf/fgpv-vpgf" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    openssl aes-256-cbc -k "$PW" -out ~/.ssh/id_rsa -in devkey.enc -d
-    echo -e "Host *\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-    chmod 600 ~/.ssh/id_rsa
-    eval `ssh-agent -s`
-    ssh-add ~/.ssh/id_rsa
+if [ "$TRAVIS_REPO_SLUG" == "AleksueiR/fgpv-vpgf" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    #openssl aes-256-cbc -k "$PW" -out ~/.ssh/id_rsa -in devkey.enc -d
+    #echo -e "Host *\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+    #chmod 600 ~/.ssh/id_rsa
+    #eval `ssh-agent -s`
+    #ssh-add ~/.ssh/id_rsa
 
     if [ -n "$TRAVIS_TAG" ]; then
         DESTDIR="$DESTDIR/$TRAVIS_TAG/"
@@ -14,7 +14,7 @@ if [ "$TRAVIS_REPO_SLUG" == "fgpv-vpgf/fgpv-vpgf" -a "$TRAVIS_PULL_REQUEST" == "
         FILES=$(find build -type f)
         for f in $FILES;
         do
-          # echo "Processing $f file..."
+          echo "Processing $f file..."
           # uploading to blob
           azure storage blob upload $f rock "$TRAVIS_TAG/$f"
         done
@@ -23,7 +23,7 @@ if [ "$TRAVIS_REPO_SLUG" == "fgpv-vpgf/fgpv-vpgf" -a "$TRAVIS_PULL_REQUEST" == "
         DESTDIR="$DESTDIR/$TRAVIS_BRANCH/"
     fi
 
-    echo "Destintation: $DESTDIR"
-    ssh "$SSHSRV" mkdir -p "$DESTDIR"
-    rsync -av --delete "build/" "$SSHSRV:$DESTDIR"
+    #echo "Destintation: $DESTDIR"
+    #ssh "$SSHSRV" mkdir -p "$DESTDIR"
+    #rsync -av --delete "build/" "$SSHSRV:$DESTDIR"
 fi
